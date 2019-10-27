2754da3353710f1006be1c3825972aa55224197c
Davis <dave.slim9@gmail.com>
2019-10-27 19:57:51 +0100

    Imported ProductService

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,4 +1,7 @@
+import { ProductService } from '../product.service';
 import { Component, OnInit } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-product-description',
@@ -7,7 +10,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDescriptionComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _productService) { }
 
   ngOnInit() {
   }
