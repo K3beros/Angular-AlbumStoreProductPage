8c79d70876238d8aae871c62ec9c412b35706470
Davis <dave.slim9@gmail.com>
2019-10-30 08:34:32 +0100

    Half way through mounting product-list component

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,6 +3,7 @@ import { Http, Response } from "@angular/http";
 import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs';
 import { Album } from './album';
+import { Product } from './product';
 
 
 
@@ -11,9 +12,14 @@ import { Album } from './album';
 @Injectable()
 export class ProductService {
 
+  private _productsUrl = "../assets/products.json"
   private _albumUrl = "../assets/album.json"
   constructor(private _http: Http) { }
   getAlbum( id: number ):Observable<Album> {
     return this._http.get(this._albumUrl).map((response) => <Album> response.json());
+  }
+
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl).map(response => <Product[]>response.json())
   }
 }
